1fad0fc69a4948a8ff87806db511d0dabcce2c24
Aditi Nair Balachandran <71278983+AditiNairB@users.noreply.github.com>
2020-10-13 15:45:12 -0400

    some unexpected files to be committed

--- personal-budget/src/app/homepage/homepage.component.ts
+++ personal-budget/src/app/homepage/homepage.component.ts
@@ -39,7 +39,7 @@ export class HomepageComponent implements OnInit {
         this.dataSource.datasets[0].data[i] = res.myBudget[i].budget;
         this.dataSource.labels[i] = res.myBudget[i].title;
     }
-    this.createChart();
+      this.createChart();
     });
   }
 
